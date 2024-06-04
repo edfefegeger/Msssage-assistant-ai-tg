093deed166ef34ad6e92beb1732dfe8f8ca78697
MYXAMOR338 <silova155@gmail.com>
2024-06-04 23:24:06 +0300

    оповещение по телеграмму

--- logger.py
+++ logger.py
@@ -1,7 +1,16 @@
 import logging
+import telebot
+from telebot import types
+import configparser
 
+config = configparser.ConfigParser()
+config.read('congig.conf', encoding='utf-8')
+TELEGRAM_BOT_TOKEN = config['API']['notify_bot']
 
-# Конфигурация логгера
+CHAT_ID = '783897764'
+CHAT_ID2 = '783897764'
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+
 logging.basicConfig(filename='LOGS.log', level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s', encoding='utf-8')
 
 
@@ -14,3 +23,7 @@ def log_and_print(*messages):
     else:
         logging.warn(formatted_message)
         print(formatted_message)
+        bot.send_message(CHAT_ID, formatted_message)
+        if CHAT_ID2!= CHAT_ID:
+            bot.send_message(CHAT_ID2, formatted_message)
+#226308337
